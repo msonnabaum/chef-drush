cb2515b975ea68d3c8960fe1212d84dfa5fdeb12
David King <dking@xforty.com>
2011-12-29 08:40:31 -0500

    Better formatting for metadata recipe descriptions

--- metadata.rb
+++ metadata.rb
@@ -6,9 +6,9 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.9.2"
 depends          "php"
 
-recipe           "drush", "Installs drush"
-recipe           "drush::make", "Installs drush_make, first making sure drush is installed"
-recipe           "drush::dev", "Installs master branch of drush from git"
+recipe           "drush",             "Installs drush"
+recipe           "drush::make",       "Installs drush_make, first making sure drush is installed"
+recipe           "drush::dev",        "Installs master branch of drush from git"
 recipe           "drush::drush4-dev", "Installs 7.x-4.x branch of drush from git"
 
 %w{ debian ubuntu centos }.each do |os|
