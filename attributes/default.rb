31d768a2fb8e3fd334aec03f9dde216ebe43ea3b
Patrick Connolly <patrick@myplanetdigital.com>
2012-04-05 13:35:35 -0400

    Remove alignment  in attr file whitespace

--- attributes/default.rb
+++ attributes/default.rb
@@ -22,12 +22,12 @@
 # TODO: implement preferred_state attribute and logic once pear bug is fixed
 
 # Options: pear, git
-default['drush']['install_method']  = "pear"
+default['drush']['install_method'] = "pear"
 
 # Used for drush install via git and make install (PEAR stores here by default).
-default['drush']['install_dir']     = "/usr/share/php/drush"
+default['drush']['install_dir'] = "/usr/share/php/drush"
 
 # When installing via PEAR, this is the x.y.z pear version (eg. 4.5.0)
 # When installing via Git, this is a commit/tag/branch reference (eg. 6e4c1e22f0b / 7.x-4.5 / 7.x-4.x)
-default['drush']['version']         = "5.0.0"
+default['drush']['version'] = "5.0.0"
 default['drush']['make']['version'] = "2.3"
