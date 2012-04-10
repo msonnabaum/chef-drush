c7d7f34fe15dfdc0bac50746ce9c6859e0b650b8
Patrick Connolly <patrick@myplanetdigital.com>
2012-04-10 11:17:19 -0400

    Bumped version to 0.10.0

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "mark.sonnabaum@acquia.com"
 license          "Apache 2.0"
 description      "Installs drush"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.9.2"
+version          "0.10.0"
 depends          "php"
 recommends       "git"
 suggests         "subversion"
