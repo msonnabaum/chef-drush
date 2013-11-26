6a1f0b28741d58ca04333220bd59fed3d7b75e44
M Parker <mparker17@536298.no-reply.drupal.org>
2013-11-25 20:17:53 -0500

    Add correct version dependency.
    Support for pear was added in the php cookbook version 1.1.0.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ license          "Apache 2.0"
 description      "Installs drush"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.10.0"
-depends          "php"
+depends          "php", "~> 1.1.0"
 recommends       "git"
 suggests         "subversion"
 
