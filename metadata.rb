c6deb8fa8f4e23c628e3e5cad340642a34f7be16
M Parker <mparker17@536298.no-reply.drupal.org>
2013-11-25 20:02:53 -0500

    Add cookbook name, better description to metadata.

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,8 @@
+name             'drush'
 maintainer       "Mark Sonnabaum"
 maintainer_email "mark.sonnabaum@acquia.com"
 license          "Apache 2.0"
-description      "Installs drush"
+description      "Installs drush, the Drupal Shell."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.10.0"
 depends          "php"
